e1a65c831cc3076a933a6b0198196a83549a618e
Sam Wen <samw@ritani.com>
2022-06-28 07:25:12 -0400

    refactory code

--- server/pfapi/index.js
+++ server/pfapi/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-//const pfapi = require('/Users/sam/iamsamwen/strapi-pfapi/src');
-const pfapi = require('strapi-pfapi');
+const pfapi = require('/Users/sam/iamsamwen/strapi-pfapi/src');
+//const pfapi = require('strapi-pfapi');
 
 module.exports = {
     ...pfapi
@@ -22,15 +22,22 @@ class PfapiApp extends AppBase {
         this.config = default_config;
     }
 
+    is_white_listed(ctx) {
+        if (this.config.white_ips_list) {
+            const white_ips_list = this.app.config.white_ips_list;
+            return is_ip_matched(ctx, white_ips_list);
+        }
+        return false;
+    }
+
     is_blocked(ctx) {
         const block_ips_list = this.config.block_ips_list;
         return is_ip_matched(ctx, block_ips_list);
     }
 
-    is_throttled(ctx) {
-        if (!this.throttle) return false;
-        return this.throttle.is_throttled(ctx);
-    }    
+    throttle_pattern(ctx) {
+        return {ip: ctx.ip};
+    }   
 
     is_auth(ctx, {api_key, uid}) {
         if (this.config.api_keys) {
@@ -51,60 +58,20 @@ class PfapiApp extends AppBase {
         }
     }
 
-    del_config(key, is_handle) {
-
-        if (!this.local_cache || !key) return false;
-
-        const cache_key = get_config_key(key, is_handle);
-        this.local_cache.delete(cache_key);
-
-        if (key === this.constructor.name) {
-            this.config = default_config;
-            this.apply_config();
-        }
-    }
-
-    update_config(item) {
-        if (!this.local_cache || !item) return false;
-        const key = get_item_config_key(item);
-        if (!key) return false;
-        const data = normalize_data(item);
-        //console.log(item.key || item.handle, key, data);
-        if (item.key === this.constructor.name) {
-            Object.assign(this.config, data);
-            this.apply_config();
-            return true;
-        }
-        return this.local_cache.put(key, data, true);
-    }
-
-    apply_config() {
-        this.strapi.app.proxy = !!this.config.proxy;
-        this.throttle.apply_rate_limits(this.config.rate_limits)
-    }
-
     async update_configs() {
 
         if (!this.strapi) return;
 
-        const query = {where: {}};
-        if (this.updated_at) query.where.updatedAt = {$gt: this.updated_at};
-
-        let now = new Date();
-
-        const items1 = await this.strapi.query(config_uid).findMany(query);
-        const items2 = await this.strapi.query(handle_uid).findMany(query);
+        const items1 = await this.strapi.query(config_uid).findMany();
+        const items2 = await this.strapi.query(handle_uid).findMany();
 
         const items = [...items1, ...items2];
 
         if (items.length > 0) {
             for (const item of items) this.update_config(item);
-        } else if (!this.updated_at) {
+        } else {
             await this.initialize_data();
-            now = new Date();
         }
-
-        this.updated_at = now;    
     }
 
     async initialize_data() {
